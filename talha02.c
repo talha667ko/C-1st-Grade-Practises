3461a97e92de2e69e8061aea22d3af7c3db28ec4
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-10-25 15:11:19 +0300

    Add files via upload

---
+++ talha02.c
@@ -0,0 +1,17 @@
+// Talha Korkmaz 230202010
+
+#include<stdio.h>
+
+ int main()
+{
+    char a = 'A';
+    char c = 'B';
+    int d = 1;
+    int e = 2;
+    char f = 'Z';
+    char g = 'z';
+    float h = 12.36;
+    int i = 8;
+    printf("%c%c\n%c%d\n%d%c\n%c%c %f %d", a, a, c, d, e, a, f, g, h, i);
+    return 0;
+}
