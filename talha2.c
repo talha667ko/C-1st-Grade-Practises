25f36b0a7ca2d3b4406ac8885a11e4d36a22add5
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-10-31 18:24:22 +0300

    Add files via upload

---
+++ talha2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int x = 0;
+    float y = 0;
+    printf("Urunun fiyatini giriniz : ");
+    scanf("%d", &x);
+    if (0 <= x < 100){
+    y = x*0,9;
+    }
+    else if (100 <= x < 500){
+    y = x*0,85;
+    }
+    else if(500 <= x){
+    y = x*0,75;
+    }
+    printf("Urunun indirimli fiyati = %f", y);
+    return 0;
+}
