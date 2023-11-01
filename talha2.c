45b66b54bb33dc481177e7e837836e6cd89768ad
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-01 10:16:51 +0300

    Update talha2.c

--- talha2.c
+++ talha2.c
@@ -11,13 +11,13 @@ int main()
     scanf("%d", &x);
     
     if (0 <= x < 100){
-    y = x*0,9;
+    y = x*0.9;
     }
     else if (100 <= x < 500){
-    y = x*0,85;
+    y = x*0.85;
     }
     else if(500 <= x){
-    y = x*0,75;
+    y = x*0.75;
     }
     
     printf("Urunun indirimli fiyati = %f", y);
