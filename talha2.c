aab05da3e2b926c96000b358543e5cd6c2cc1ab3
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-01 11:21:19 +0300

    talha2.c

--- talha2.c
+++ talha2.c
@@ -12,13 +12,13 @@ int main()
     
     if (0 <= x < 100){
     y = x*0.9;
-    }
+        }
     else if (100 <= x < 500){
     y = x*0.85;
-    }
+        }
     else if(500 <= x){
     y = x*0.75;
-    }
+        }
     
     printf("Urunun indirimli fiyati = %f", y);
     
