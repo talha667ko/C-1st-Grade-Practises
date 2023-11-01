ab3c501954a61011ea253d6a669e9d20bca299f4
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-01 14:46:29 +0300

    talha2.c

--- talha2.c
+++ talha2.c
@@ -2,28 +2,24 @@
 #include <stdlib.h>
 
 int main()
-
 {
-    int x = 0;
+     float x = 0;
     float y = 0;
-    
-    printf("Urunun fiyatini giriniz : ");
-    scanf("%d", &x);
-    
+
+    printf("Urunun fiyatini giriniz(TL) : ");
+    scanf("%f", &x);
+
     if (0 <= x < 100){
     y = x*0.9;
         }
     else if (100 <= x < 500){
     y = x*0.85;
         }
-    else if(500 <= x){
+     else {
     y = x*0.75;
         }
-     else {
-        printf("Dogru bir deger girmediniz, Tekrar deneyiniz.");
-    }
-    
+
     printf("Urunun indirimli fiyati = %f\n", y);
-    
+
     return 0;
 }
