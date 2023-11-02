a048c5ac6243e8f48d345ae0b5ca4f78f9156428
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-02 14:17:27 +0300

    talha2.c
    final and working one FİNALLY !!!!!!!!

--- talha2.c
+++ talha2.c
@@ -9,17 +9,26 @@ int main()
     printf("Urunun fiyatini giriniz(TL) : ");
     scanf("%f", &x);
 
-    if (0 <= x < 100){
-    y = x*0.9;
-        }
-    else if (100 <= x < 500){
-    y = x*0.85;
-        }
-     else {
-    y = x*0.75;
-        }
+       if (0<=x){
+
+    if (0<=x, x<100){
+        y = x*0.9;
+    }
+    else if (100<=x, x<500){
+        y = x*0.85;
+    }
+    else{
+        y = x*0.75;
+    }
 
     printf("Urunun indirimli fiyati = %f\n", y);
+       }
+
+        else{
+            printf("Yanlis bir deger girdiniz, tekrar deneyiniz!");
+        }
 
     return 0;
 }
+
+//667
