bc19d781837f8b140a7b5e8cec7a83922d67636d
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-06 12:21:08 +0300

    Add files via upload

---
+++ talha4.c
@@ -0,0 +1,96 @@
+//
+//  main.c
+//  dene
+//
+//  Created by Talha KORKMAZ on 05/11/2023.
+//667
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+//    -EXERCISE 6.8
+//    int sayi1;
+//    int sayi2;
+//    printf("Lutfen birinci sayiyi giriniz: ");
+//    scanf("%d", &sayi1);
+//    printf("Lutfen ikinci sayiyi giriniz: ");
+//    scanf("%d", &sayi2);
+//    printf("---------------------------------");
+//    printf("\nGirilen %d ve %d sayilarinin toplami %d dir.\n", sayi1, sayi2, sayi1+sayi2);
+    
+//    -EXERCISE 6.9
+//    int yaricap;
+//    int aci;
+//    float pi= 3.14;
+//    printf("Yaricap degerini giriniz: ");
+//    scanf("%d", &yaricap);
+//    printf("Merkez aci degerini giriniz: ");
+//    scanf("%d", &aci);
+//    printf("\nAcinin taradigi alan %.2f santimetrekaredir\n", pi*yaricap*yaricap*aci/360);
+//    
+    
+//    -EXERCISE 6.10
+//    float lt;
+//    float TL;
+//    int km;
+//    int maliyet;
+//    printf("Km basina yakit tuketimi (lt)   ");
+//    scanf("%f", &lt);
+//    printf("1 lt yakitin fiyati (TL)   ");
+//    scanf("%f", &TL);
+//    printf("Aricin katettigi toplam yol (km)   ");
+//    scanf("%d", &km);
+//    maliyet = lt*TL*km;
+//    printf("\nToplam yakit maliyeti %8.d\n", maliyet);
+    
+//    -EXERCISE 6.11
+//    int cap;
+//    float perimeter;
+//    float pi = 3.14;
+//    printf("Cemberin capini giriniz: ");
+//    scanf("%d", &cap);
+//    perimeter = cap*pi;
+//    printf("\nCemberin uzunlugu: %.f\n", perimeter);
+    
+//    -EXERCISE 6.12
+//    float mil;
+//    float km;
+//    printf("Mil olarak uzakligi giriniz: ");
+//    scanf("%f", &mil);
+//    km = mil*1.609;
+//    printf("\nGirilen %.2f mil, %.2f km dir.\n", mil, km);
+    
+//    -EXERCISE 6.15
+//    int dogum;
+//    int simdi;
+//    printf("Dogum yilinizi giriniz: ");
+//    scanf("%d", &dogum);
+//    printf("Gunumuz yilini giriniz: ");
+//    scanf("%d", &simdi);
+//    printf("\nYasiniz: %d\n", simdi-dogum);
+      
+//    -EXERCISE 6.16
+//    float tamsayiA;
+//    float tamsayiB;
+//    printf("Harmonik ortalama alincak degerler? : ");
+//    scanf("%f %f", &tamsayiA, &tamsayiB);
+//    printf("\nHarmonik ortalama = %.2f\n", (2*tamsayiA*tamsayiB)/(tamsayiA+tamsayiB));
+    
+//    -EXERCISE 6.17 ***
+//    int sure;
+//    int saat;
+//    int dakika;
+//    int saniye;
+//    printf("Sureyi giriniz (Saniye olarak): ");
+//    scanf("%d", &sure);
+//    saat = sure/3600;
+//    dakika = (sure%3600)/60;
+//    saniye = (sure%3600)%60;
+//    printf("\nBu sure %d saat, %d dakika, %d saniye.\n", saat, dakika, saniye);
+    
+//
+    
+    
+    
+    return 0;
+}
