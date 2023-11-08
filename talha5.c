521b8bf3d69fd05396bb5282a3598b0a564d3fcd
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-08 13:09:23 +0300

    Add files via upload

---
+++ talha5.c
@@ -0,0 +1,72 @@
+//
+//  main.c
+//  dene
+//
+//  Created by TALHA Korkmaz on 08/11/2023.
+//667
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+//    -EXERCISE 7.1
+//    int araSinav;
+//    int sonSinav;
+//    int devamPuan;
+//    float ortalama;
+//    printf("Ara sinav puanini giriniz: ");
+//    scanf("%d", &araSinav);
+//    printf("Son sinav puanini giriniz: ");
+//    scanf("%d", &sonSinav);
+//    printf("Devam puanini giriniz: ");
+//    scanf("%d", &devamPuan);
+//    ortalama = araSinav*0.4 + sonSinav*0.5 + devamPuan*0.1;
+//    printf("\nOrtalama puaniniz %.2f", ortalama);
+//    if (ortalama >= 60) {
+//        printf("\nTebrikler gectiniz !\n");
+//    }
+//    else
+//        printf("\nMaalesef kaldiniz !\n");
+
+//    -EXERCICE 7.3 not done
+//    int number1;
+//    int number2;
+//    int number0;
+//    int number3;
+//    int number4;
+//    printf("Bes basamakli sayi giriniz : ");
+//    scanf("%d%d%d%d%d", &number1,&number2,&number0,&number3,&number4);
+//    if (number1==number3 && number2==number4) {
+//        printf("\nGirilen %d%d%d%d%d sayisi palindrome dur.\n", number1, number2, number0, number3, number4);
+//    }
+//    else
+//        printf("\nGirilen %d%d%d%d%d sayisi palindrome degildir.\n", number1, number2, number0, number3, number4);
+    
+//    -EXERCISE 7.4
+//    float trafik;
+//    float motor;
+//    float ilkyardim;
+//    float trfkPuan;
+//    float mtrPuan;
+//    float ilkyrdmPuan;
+//    printf("Trafik yanlis sayisini giriniz: ");
+//    scanf("%f", &trafik);
+//    printf("Motor yanlis sayisini giriniz: ");
+//    scanf("%f", &motor);
+//    printf("Ilkyardim yanlis sayisini giriniz: ");
+//    scanf("%f", &ilkyardim);
+//    trfkPuan = (50-trafik)*2;
+//    mtrPuan = (40-motor)*5/2;
+//    ilkyrdmPuan = (30-ilkyardim)*10/3;
+//    printf("\nTrafik puaniniz: %.2f", trfkPuan);
+//    printf("\nMotor puaninz: %.2f", mtrPuan);
+//    printf("\nIlkyardim puaniniz: %.2f\n", ilkyrdmPuan);
+//    if (trfkPuan >= 70 && mtrPuan >= 70 && ilkyrdmPuan >= 70) {
+//        printf("Direksiyon sinavina hak kazandiniz!");
+//    }
+//    else
+//        printf("Uzgunuz direksiyon sinavina katilamazsiniz.\n");
+    
+    
+    return 0;
+}
