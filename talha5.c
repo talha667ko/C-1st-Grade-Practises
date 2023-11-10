940b0ff845d199d4f5c617fff301c9c9238f5d9a
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-10 17:56:32 +0300

    talha5.c

--- talha5.c
+++ talha5.c
@@ -14,19 +14,25 @@ int main(int argc, const char * argv[]) {
 //    int sonSinav;
 //    int devamPuan;
 //    float ortalama;
+//    
 //    printf("Ara sinav puanini giriniz: ");
 //    scanf("%d", &araSinav);
 //    printf("Son sinav puanini giriniz: ");
 //    scanf("%d", &sonSinav);
 //    printf("Devam puanini giriniz: ");
 //    scanf("%d", &devamPuan);
+//    
 //    ortalama = araSinav*0.4 + sonSinav*0.5 + devamPuan*0.1;
+//
 //    printf("\nOrtalama puaniniz %.2f", ortalama);
+//    
 //    if (ortalama >= 60) {
 //        printf("\nTebrikler gectiniz !\n");
 //    }
 //    else
 //        printf("\nMaalesef kaldiniz !\n");
+
+    
 
 //    -EXERCICE 7.3 not done
 //    int number1;
@@ -34,13 +40,17 @@ int main(int argc, const char * argv[]) {
 //    int number0;
 //    int number3;
 //    int number4;
+//    
 //    printf("Bes basamakli sayi giriniz : ");
 //    scanf("%d%d%d%d%d", &number1,&number2,&number0,&number3,&number4);
+//    
 //    if (number1==number3 && number2==number4) {
 //        printf("\nGirilen %d%d%d%d%d sayisi palindrome dur.\n", number1, number2, number0, number3, number4);
 //    }
 //    else
 //        printf("\nGirilen %d%d%d%d%d sayisi palindrome degildir.\n", number1, number2, number0, number3, number4);
+
+
     
 //    -EXERCISE 7.4
 //    float trafik;
@@ -49,23 +59,29 @@ int main(int argc, const char * argv[]) {
 //    float trfkPuan;
 //    float mtrPuan;
 //    float ilkyrdmPuan;
+//    
 //    printf("Trafik yanlis sayisini giriniz: ");
 //    scanf("%f", &trafik);
 //    printf("Motor yanlis sayisini giriniz: ");
 //    scanf("%f", &motor);
 //    printf("Ilkyardim yanlis sayisini giriniz: ");
 //    scanf("%f", &ilkyardim);
+//    
 //    trfkPuan = (50-trafik)*2;
 //    mtrPuan = (40-motor)*5/2;
 //    ilkyrdmPuan = (30-ilkyardim)*10/3;
+//    
 //    printf("\nTrafik puaniniz: %.2f", trfkPuan);
 //    printf("\nMotor puaninz: %.2f", mtrPuan);
 //    printf("\nIlkyardim puaniniz: %.2f\n", ilkyrdmPuan);
+//    
 //    if (trfkPuan >= 70 && mtrPuan >= 70 && ilkyrdmPuan >= 70) {
 //        printf("Direksiyon sinavina hak kazandiniz!");
 //    }
 //    else
 //        printf("Uzgunuz direksiyon sinavina katilamazsiniz.\n");
+
+
     
 //    -EXERCISE 7.5
 //    int yas;
@@ -95,6 +111,8 @@ int main(int argc, const char * argv[]) {
 //    else
 //    printf("Maalesef askeri liseye giremezsiniz.\n");
 
+
+    
 //    -EXERCISE 7.7
 //    char gender;
 //    float height;
