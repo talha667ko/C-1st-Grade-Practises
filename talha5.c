2da7f4453c29d561d53e23310da235caeca8c608
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-15 09:13:16 +0300

    Update talha5.c

--- talha5.c
+++ talha5.c
@@ -11,270 +11,269 @@
 int main(int argc, const char * argv[]) {
 
 //    -EXERCISE 7.1
-//    int araSinav;
-//    int sonSinav;
-//    int devamPuan;
-//    float ortalama;
-//    
-//    printf("Ara sinav puanini giriniz: ");
-//    scanf("%d", &araSinav);
-//    printf("Son sinav puanini giriniz: ");
-//    scanf("%d", &sonSinav);
-//    printf("Devam puanini giriniz: ");
-//    scanf("%d", &devamPuan);
-//    
-//    ortalama = araSinav*0.4 + sonSinav*0.5 + devamPuan*0.1;
-//
-//    printf("\nOrtalama puaniniz %.2f", ortalama);
-//    
-//    if (ortalama >= 60) {
-//        printf("\nTebrikler gectiniz !\n");
-//    }
-//    else
-//        printf("\nMaalesef kaldiniz !\n");
+    /*int araSinav;
+    int sonSinav;
+    int devamPuan;
+    float ortalama;
+    
+    printf("Ara sinav puanini giriniz: ");
+    scanf("%d", &araSinav);
+    printf("Son sinav puanini giriniz: ");
+    scanf("%d", &sonSinav);
+    printf("Devam puanini giriniz: ");
+    scanf("%d", &devamPuan);
+    
+    ortalama = araSinav*0.4 + sonSinav*0.5 + devamPuan*0.1;
+
+    printf("\nOrtalama puaniniz %.2f", ortalama);
+    
+    if (ortalama >= 60) {
+        printf("\nTebrikler gectiniz !\n");
+    }
+    else
+        printf("\nMaalesef kaldiniz !\n");*/
 
     
 
 //    -EXERCICE 7.3 not done
-//    int number1;
-//    int number2;
-//    int number0;
-//    int number3;
-//    int number4;
-//    
-//    printf("Bes basamakli sayi giriniz : ");
-//    scanf("%d%d%d%d%d", &number1,&number2,&number0,&number3,&number4);
-//    
-//    if (number1==number3 && number2==number4) {
-//        printf("\nGirilen %d%d%d%d%d sayisi palindrome dur.\n", number1, number2, number0, number3, number4);
-//    }
-//    else
-//        printf("\nGirilen %d%d%d%d%d sayisi palindrome degildir.\n", number1, number2, number0, number3, number4);
+      /*int number1;
+        int number2;
+        int number0;
+        int number3;
+        int number4;
+    
+        printf("Bes basamakli sayi giriniz : ");
+        scanf("%d%d%d%d%d", &number1,&number2,&number0,&number3,&number4);
+    
+        if (number1==number3 && number2==number4) {
+            printf("\nGirilen %d%d%d%d%d sayisi palindrome dur.\n", number1, number2, number0, number3, number4);
+        }
+        else
+            printf("\nGirilen %d%d%d%d%d sayisi palindrome degildir.\n", number1, number2, number0, number3, number4);*/
 
 
     
 //    -EXERCISE 7.4
-//    float trafik;
-//    float motor;
-//    float ilkyardim;
-//    float trfkPuan;
-//    float mtrPuan;
-//    float ilkyrdmPuan;
-//    
-//    printf("Trafik yanlis sayisini giriniz: ");
-//    scanf("%f", &trafik);
-//    printf("Motor yanlis sayisini giriniz: ");
-//    scanf("%f", &motor);
-//    printf("Ilkyardim yanlis sayisini giriniz: ");
-//    scanf("%f", &ilkyardim);
-//    
-//    trfkPuan = (50-trafik)*2;
-//    mtrPuan = (40-motor)*5/2;
-//    ilkyrdmPuan = (30-ilkyardim)*10/3;
-//    
-//    printf("\nTrafik puaniniz: %.2f", trfkPuan);
-//    printf("\nMotor puaninz: %.2f", mtrPuan);
-//    printf("\nIlkyardim puaniniz: %.2f\n", ilkyrdmPuan);
-//    
-//    if (trfkPuan >= 70 && mtrPuan >= 70 && ilkyrdmPuan >= 70) {
-//        printf("Direksiyon sinavina hak kazandiniz!");
-//    }
-//    else
-//        printf("Uzgunuz direksiyon sinavina katilamazsiniz.\n");
+     /*float trafik;
+        float motor;
+        float ilkyardim;
+        float trfkPuan;
+        float mtrPuan;
+        float ilkyrdmPuan;
+    
+        printf("Trafik yanlis sayisini giriniz: ");
+        scanf("%f", &trafik);
+        printf("Motor yanlis sayisini giriniz: ");
+        scanf("%f", &motor);
+        printf("Ilkyardim yanlis sayisini giriniz: ");
+        scanf("%f", &ilkyardim);
+    
+        trfkPuan = (50-trafik)*2;
+        mtrPuan = (40-motor)*5/2;
+        ilkyrdmPuan = (30-ilkyardim)*10/3;
+    
+        printf("\nTrafik puaniniz: %.2f", trfkPuan);
+        printf("\nMotor puaninz: %.2f", mtrPuan);
+        printf("\nIlkyardim puaniniz: %.2f\n", ilkyrdmPuan);
+    
+        if (trfkPuan >= 70 && mtrPuan >= 70 && ilkyrdmPuan >= 70) {
+            printf("Direksiyon sinavina hak kazandiniz!");
+        }
+        else
+            printf("Uzgunuz direksiyon sinavina katilamazsiniz.\n");*/
 
 
     
 //    -EXERCISE 7.5
-//    int yas;
-//    float kilo;
-//    float boy;
-//    float vucutKitleİ;
-//
-//    printf("Yasinizi giriniz: ");
-//    scanf("%d", &yas);
-//
-//    if(yas<= (17) || yas>=13){
-//
-//        printf("Kilonuzu giriniz (kg) : ");
-//        scanf("%f", &kilo);
-//        printf("Boyunuzu giriniz (m) : ");
-//        scanf("%f", &boy);
-//
-//        vucutKitleİ = kilo/(boy*boy);
-//
-//        if(vucutKitleİ>=18.50 || vucutKitleİ<=24.99){
-//        printf("\nVucut kitle indeksiniz: %.2f", vucutKitleİ);
-//        printf("\nTebrikler askeri liseye girebilirsiniz.\n");
-//        }
-//        else
-//            printf("Maalesef liseye giremezsinz.\n");
-//    }
-//    else
-//    printf("Maalesef askeri liseye giremezsiniz.\n");
+        /*int yas;
+        float kilo;
+        float boy;
+        float vucutKitleİ;
+    
+        printf("Yasinizi giriniz: ");
+        scanf("%d", &yas);
+    
+        if(yas<= (17) || yas>=13){
+    
+            printf("Kilonuzu giriniz (kg) : ");
+            scanf("%f", &kilo);
+            printf("Boyunuzu giriniz (m) : ");
+            scanf("%f", &boy);
+    
+            vucutKitleİ = kilo/(boy*boy);
+    
+            if(vucutKitleİ>=18.50 || vucutKitleİ<=24.99){
+            printf("\nVucut kitle indeksiniz: %.2f", vucutKitleİ);
+            printf("\nTebrikler askeri liseye girebilirsiniz.\n");
+            }
+            else
+                printf("Maalesef liseye giremezsinz.\n");
+        }
+        else
+        printf("Maalesef askeri liseye giremezsiniz.\n");*/
 
 
     
 //    -EXERCISE 7.7
-//    char gender;
-//    float height;
-//    float weight;
-//    float ideal;
-//    float centimeter;
-//    
-//    printf("Enter your gender (m, M - f, F): ");
-//    scanf("%c", &gender);
-//    
-//    if (gender== 'm' || gender== 'M' ) {
-//        printf("Enter your height (m): ");
-//        scanf("%f", &height);
-//        printf("Enter your weight (kg): ");
-//        scanf("%f", &weight);
-//        
-//        centimeter = (height*100) - 150;
-//        ideal = 48.00 + centimeter/2.5*2.7;
-//        
-//        printf("\nYour ideal weight: %.2f\n", ideal);
-//        if (weight > ideal) {
-//            printf("You're heavier than your ideal weight.\n");
-//        }
-//        else if (weight < ideal){
-//            printf("\nYou're lighter than your ideal weight.\n");
-//        }
-//        else
-//            printf("You're at your ideal weight!\n");
-//    }
-//    else if (gender== 'f' || gender== 'F'){
-//        
-//        printf("Enter your height (m): ");
-//        scanf("%f", &height);
-//        printf("Enter your weight (kg): ");
-//        scanf("%f", &weight);
-//        
-//        centimeter = (height*100) - 150;
-//        ideal = 45.00 + centimeter/2.5*2.2;
-//        
-//        printf("\nYour ideal weight: %.2f\n", ideal);
-//        if (weight > ideal) {
-//            printf("You're heavier than your ideal weight.\n");
-//        }
-//        else if (weight < ideal){
-//            printf("\nYou're lighter than your ideal weight.\n");
-//        }
-//        else
-//            printf("You're at your ideal weight!\n");
-//        
-//    }
-//        else
-//            printf("You've entered a wrong letter.\n");
-
+      /*char gender;
+        float height;
+        float weight;
+        float ideal;
+        float centimeter;
+    
+        printf("Enter your gender (m, M - f, F): ");
+        scanf("%c", &gender);
+    
+        if (gender== 'm' || gender== 'M' ) {
+            printf("Enter your height (m): ");
+            scanf("%f", &height);
+            printf("Enter your weight (kg): ");
+            scanf("%f", &weight);
+    
+            centimeter = (height*100) - 150;
+            ideal = 48.00 + centimeter/2.5*2.7;
+    
+            printf("\nYour ideal weight: %.2f\n", ideal);
+            if (weight > ideal) {
+                printf("You're heavier than your ideal weight.\n");
+            }
+            else if (weight < ideal){
+                printf("\nYou're lighter than your ideal weight.\n");
+            }
+            else
+                printf("You're at your ideal weight!\n");
+        }
+        else if (gender== 'f' || gender== 'F'){
+    
+            printf("Enter your height (m): ");
+            scanf("%f", &height);
+            printf("Enter your weight (kg): ");
+            scanf("%f", &weight);
+    
+            centimeter = (height*100) - 150;
+            ideal = 45.00 + centimeter/2.5*2.2;
+    
+            printf("\nYour ideal weight: %.2f\n", ideal);
+            if (weight > ideal) {
+                printf("You're heavier than your ideal weight.\n");
+            }
+            else if (weight < ideal){
+                printf("\nYou're lighter than your ideal weight.\n");
+            }
+            else
+                printf("You're at your ideal weight!\n");
+    
+        }
+            else
+                printf("You've entered a wrong letter.\n");*/
 
 
 //    -EXERCISE 7.8
-//    int assurance;
-//    int repair;
-//    int junk;
-//    
-//    printf("Enter the car's assurance, repairs and junk prices: ");
-//    scanf("%d %d %d", &assurance, &repair, &junk);
-//    
-//    if (assurance-junk > repair) {
-//        printf("\nSend it to the car service.");
-//    }
-//    else
-//        printf("\nSend it to the car impound.\n");
-//        printf("With this move you'll save %d$\n", repair-(assurance-junk));
+     /*int assurance;
+        int repair;
+        int junk;
+    
+        printf("Enter the car's assurance, repairs and junk prices: ");
+        scanf("%d %d %d", &assurance, &repair, &junk);
+    
+        if (assurance-junk > repair) {
+            printf("\nSend it to the car service.");
+        }
+        else
+            printf("\nSend it to the car impound.\n");
+            printf("With this move you'll save %d$\n", repair-(assurance-junk));*/
 
 
 
 //    -EXERCISE 7.13
-//    int a;
-//    int b;
-//    int c;
-//    int ab;
-//    int bc;
-//    int ac;
-//    
-//    printf("Enter the values a, b , c of the triangle: ");
-//    scanf("%d %d %d", &a, &b, &c);
-//    
-//    ab = fabs(a-b);
-//    bc = fabs(b-c);
-//    ac = fabs(a-c);
-//    
-//    if (a<b+c && a>bc && b<a+c && b>ac && c<a+b && c>ab) {
-//        printf("It is a triangle. This triangle type: \n");
-//        if (b==a && a!=c && b!=c|| b==c && b!=a && c!=a || a==c && a!=b && c!=b) {
-//            printf("Isosceles triangle.\n");
-//        }
-//        else if(a==b && a==c && b==c)
-//            printf("Equilateral triangle.\n");
-//        else
-//            printf("Any triangle.\n");
-//    }
-//    else
-//        printf("It isn't a triangle.\n");
+        /*int a;
+        int b;
+        int c;
+        int ab;
+        int bc;
+        int ac;
+    
+        printf("Enter the values a, b , c of the triangle: ");
+        scanf("%d %d %d", &a, &b, &c);
+    
+        ab = fabs(a-b);
+        bc = fabs(b-c);
+        ac = fabs(a-c);
+    
+        if (a<b+c && a>bc && b<a+c && b>ac && c<a+b && c>ab) {
+            printf("It is a triangle. This triangle type: \n");
+            if (b==a && a!=c && b!=c|| b==c && b!=a && c!=a || a==c && a!=b && c!=b) {
+                printf("Isosceles triangle.\n");
+            }
+            else if(a==b && a==c && b==c)
+                printf("Equilateral triangle.\n");
+            else
+                printf("Any triangle.\n");
+        }
+        else
+            printf("It isn't a triangle.\n");*/
 
 
 //        -EXERCISE 7.15
-//    float time;
-//    
-//    printf("What time is it? : ");
-//    scanf("%f", &time);
-//    
-//    if (time>=0 && time<=24) {
-//        if (time<6)
-//            printf("Sleep good!\n");
-//        else if (time<11)
-//            printf("Good morning!\n");
-//        else if (time<17)
-//            printf("Have a good day!\n");
-//        else if (time<22)
-//            printf("Good afternoon!\n");
-//        else if (time<24)
-//            printf("Good night!\n");
-//    }
-//    else
-//        printf("You typed a wrong value.\n");
+        /*float time;
+    
+        printf("What time is it? : ");
+        scanf("%f", &time);
+    
+        if (time>=0 && time<=24) {
+            if (time<6)
+                printf("Sleep good!\n");
+            else if (time<11)
+                printf("Good morning!\n");
+            else if (time<17)
+                printf("Have a good day!\n");
+            else if (time<22)
+                printf("Good afternoon!\n");
+            else if (time<24)
+                printf("Good night!\n");
+        }
+        else
+            printf("You typed a wrong value.\n");*/
 
 
     
 //    EXAMPLE switch
-//        char vehicle;
-//    int day;
-//    int Km;
-//    int cost;
-//
-//    printf("Type of vehicle/locomotion\n");
-//    printf("(A or a) All road\n");
-//    printf("(L or l) Living vehicle\n");
-//    printf("(S or s) Sports car\n\n");
-//
-//    printf("Enter your type of vehicle/locomotion: ");
-//    scanf("%c", &vehicle);
-//    printf("How many days did you used it: ");
-//    scanf("%d", &day);
-//   printf("How many km have you done with the vehicle: ");
-//   scanf("%d", &Km);
-//
-//    switch(vehicle){
-//    case 'a':
-//    case 'A':
-//        cost = day*20 + Km*18;
-//        printf("\nYour amount is : %d $\n", cost);
-//        break;
-//    case 'l':
-//    case 'L':
-//        cost = day*32 + Km*22;
-//        printf("\nYour amount is : %d $\n", cost);
-//        break;
-//    case 's':
-//    case 'S':
-//        cost = day*51 + Km*36;
-//        printf("\nYour amount is : %d $\n", cost);
-//        break;
-//    default: printf("You entered a wrong letter!!!\n");
-//    }
-//    system("pause");
+    /*char vehicle;
+    int day;
+    int Km;
+    int cost;
+
+    printf("Type of vehicle/locomotion\n");
+    printf("(A or a) All road\n");
+    printf("(L or l) Living vehicle\n");
+    printf("(S or s) Sports car\n\n");
+
+    printf("Enter your type of vehicle/locomotion: ");
+    scanf("%c", &vehicle);
+    printf("How many days did you used it: ");
+    scanf("%d", &day);
+   printf("How many km have you done with the vehicle: ");
+   scanf("%d", &Km);
+
+    switch(vehicle){
+    case 'a':
+    case 'A':
+        cost = day*20 + Km*18;
+        printf("\nYour amount is : %d $\n", cost);
+        break;
+    case 'l':
+    case 'L':
+        cost = day*32 + Km*22;
+        printf("\nYour amount is : %d $\n", cost);
+        break;
+    case 's':
+    case 'S':
+        cost = day*51 + Km*36;
+        printf("\nYour amount is : %d $\n", cost);
+        break;
+    default: printf("You entered a wrong letter!!!\n");
+    }
+    system("pause");*/
 
 
 
@@ -396,38 +395,39 @@ int main(int argc, const char * argv[]) {
     }*/
 
 
-
-        float judge1A;
-    float judge1B;
-    float judge1C;
-    float judge2A;
-    float judge2B;
-    float judge2C;
-    float judge3A;
-    float judge3B;
-    float judge3C;
+//    EXERCISE 7.22
+    /*float contestant1[3];
+    float contestant2[3];
+    float contestant3[3];
     float pointA;
     float pointB;
     float pointC;
 
     printf("Enter the first judge's scorecard: ");
-    scanf("%f %f %f", &judge1A, &judge1B, &judge1C);
+    scanf("%f %f %f", &contestant1[0], &contestant2[0], &contestant3[0]);
     printf("Enter the second judge's scorecard: ");
-    scanf("%f %f %f", &judge2A, &judge2B, &judge2C);
+    scanf("%f %f %f", &contestant1[1], &contestant2[1], &contestant3[1]);
     printf("Enter the third judge's scorecard: ");
-    scanf("%f %f %f", &judge3A, &judge3B, &judge3C);
+    scanf("%f %f %f", &contestant1[2], &contestant2[2], &contestant3[2]);
 
-    pointA = (judge1A+judge2A+judge3A)/3;
-    pointB = (judge1B+judge2B+judge3B)/3;
-    pointC = (judge1C+judge2C+judge3C)/3;
+    pointA = (contestant1[0]+contestant1[1]+contestant1[2])/3;
+    pointB = (contestant2[0]+contestant2[1]+contestant2[2])/3;
+    pointC = (contestant3[0]+contestant3[1]+contestant3[2])/3;
 
     if(pointA>pointB && pointA>pointC)
-        printf("\nThe first contestant is is the best (His average = %.2f)", pointA);
+        printf("\nThe first contestant is is the best (His average = %.2f)\n", pointA);
     else if(pointB>pointA && pointB>pointC)
-        printf("\nThe second contestant is is the best (His average = %.2f)", pointB);
+        printf("\nThe second contestant is is the best (His average = %.2f)\n", pointB);
     else if(pointC>pointA && pointC>pointB)
         printf("\nThe third contestant is is the best (His average = %.2f)\n", pointC);
     else
-        printf("There is an equality");
+        printf("There is an equality\n");*/
+
+
+    
+
+
+
+    
     return 0;
 }
