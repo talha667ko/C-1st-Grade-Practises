39e3fd3abbf4a4814a0f2e72c71d80a72534115b
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-17 16:34:42 +0300

    talha6.c

---
+++ talha6.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+//   EXERCISE 8.1
+    /*int number;
+    
+    printf("Decimal    Octal    Hexadecimal\n");
+    
+    for (number=0; number<=15; number++) {
+        printf("%d         %o       %x\n", number, number, number);
+    }*/
+
+
+  //  EXERCISE 8.2
+    
+    
+    return 0;
+}
