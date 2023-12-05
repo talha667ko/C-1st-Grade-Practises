06e578b98cdbbad14f8627018bb56810311f5bfa
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-12-05 17:55:11 +0300

    talha6.c

--- talha6.c
+++ talha6.c
@@ -631,6 +631,47 @@ int main(int argc, const char * argv[]) {
         }
         printf("\n");
     }*/
+
+//    DRAWING AN X***
+    /*int num;
+    int i = 0;
+    int a;
+
+    printf("Enter number to draw an X: ");
+    scanf("%d", &num);
+
+    a = 2*num - 3;
+    while(i < num){
+        for(int j = 0; j < i;){
+            printf(" ");
+            if(j != i)
+                j++;
+        }
+        printf("*");
+        for(int m = 0; m < a; m++){
+            printf(" ");
+        }
+        a = a - 2;
+        if(i != num - 1)
+        printf("*");
+        
+        printf("\n");
+        i++;
+    }
+
+    int x = num - 1;
+    while(x > 0){
+        for(int b = 0; b < x - 1; b++){
+            printf(" ");
+        }
+        printf("*");
+        for(int c = 0; c < 2*(num - x) - 1; c++){
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+        x--;
+    }*/
     
     return 0;
 }
