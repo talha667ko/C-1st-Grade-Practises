65aea28036bc7462bd8c91a618f6e16e6cecd339
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-12-11 16:35:13 +0300

    Functions

--- talha7.c
+++ talha7.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void factorial(int fact){
+//     FACTORİAL W FUNCTİON
+/*void factorial(int fact){
     
     int reverse = 1;
     for (int i = 1; i <= fact; i++){
@@ -25,4 +27,35 @@ int main(){
 
     factorial(num);
     return 0;
-}
+}*/
+
+
+
+//    EXERCISE 9.1
+    /*double force(double, double, double);
+    int main()
+    {
+
+        double objt1;
+        double objt2;
+        double distance;
+
+        printf("Enter the objects' weights (kg): ");
+        scanf("%lf %lf", &objt1, &objt2);
+        printf("Enter the distance between the objects (m): ");
+        scanf("%lf", &distance);
+
+        printf("\nThe gravitational between these two object is: %f", force(objt1, objt2, distance));
+
+        return 0;
+    }
+    double force(double M1, double M2, double R)
+    {
+        float result;
+        double G = 6.673*pow(10.0,-11.0);
+        result = G*((M1*M2)/(R*R) );
+        return result;
+    }*/
+
+
+//
