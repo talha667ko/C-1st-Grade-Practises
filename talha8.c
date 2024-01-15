89ada4aa3d412aaf979abcad3166f19e7df71156
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2024-01-15 17:55:28 +0300

    Update talha8.c

--- talha8.c
+++ talha8.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <time.h>
 
-int main()
+/*int main()
 {
     float average = 0;
     int notes[20];
@@ -24,4 +24,24 @@ int main()
             printf("%d\n", notes[i]);
         }
     }
-}
+}*/
+
+
+/*int main()
+{
+    int array[5] = {13, 12, 0, 8, 5};
+
+    for(int a = 0; a < 5; a++){
+    for (int i = 0; i < 5; i++){
+        int plus;
+            if (array[i] > array[i + 1]){
+                plus = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = plus;
+            }
+        }
+        
+    }
+    printf("%d %d %d %d %d", array[0], array[1], array[2], array[3], array[4]);
+    
+}*/
