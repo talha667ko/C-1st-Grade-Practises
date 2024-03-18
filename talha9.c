cb6965ff5eda5774e6f53e6539341a7da6a27135
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2024-03-18 15:01:09 +0300

    Update talha9.c

--- talha9.c
+++ talha9.c
@@ -37,3 +37,53 @@ int main()
     printf("Sorted: %.2f, %.2f, %.2f.", a, b, c);
     return 0;
 }*/
+
+
+//    EXERCISE 12.2
+/*#define N 3
+int searching(int *vP, int ar[][N], int *rP, int *cP)
+{
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if(ar[i][j] == *vP){
+                *rP = i;
+                *cP = j;
+                return 1;
+            }
+        }
+        
+    }
+    return 0;
+}
+
+void produce(int ar[][N])
+{
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            ar[i][j] = rand() % 11;
+        }
+        
+    }
+    
+}
+int main()
+{
+    srand(time(NULL));
+    int value;
+    int row;
+    int column;
+    int array[N][N] = {0};
+
+    produce(array);
+    printf("Enter the value that you're searching: ");
+    scanf("%d", &value);
+
+    if(searching(&value, array,&row,&column) == 1){
+        printf("Your value is in the matrix.");
+        printf("\nYour value is at the %d. row and %d. column.", row, column);
+    }
+    else
+    printf("Your value isn't in the matrix.");
+
+}*/
