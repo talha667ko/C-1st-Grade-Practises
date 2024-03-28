46dd2be4a16e082489e5053bd4f6b1372fb63858
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2024-03-28 09:17:36 +0300

    Create talha10.c

---
+++ talha10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+
+//  EXERCISE 13.1
+/*int main()
+{
+    char name[255];
+    printf("Enter your name: ");
+    fgets(name,255,stdin);
+    printf("\n%s", name);
+    
+    return 0;
+}*/
