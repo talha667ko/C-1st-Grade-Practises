e182733bc1e482ea046d42ce6b2f92fb8543dc41
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2024-05-07 09:30:49 +0300

    Create talha11.c

---
+++ talha11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+
+struct student
+{
+    char name[10];
+    int age;
+    char gender;
+    int num;
+
+}a[20];
+
+int main()
+{
+    int studentsN;
+    printf("How many students are you going to enter (max:20): ");
+    scanf("%d",&studentsN);
+    for (int i = 0; i < studentsN; i++)
+    {
+        printf("Enter the name: ");
+        fflush(stdin);
+        scanf("%s", &a[i].name);
+        printf("How old is the student: ");
+        scanf("%d", &a[i].age);
+        printf("The student's gender: ");
+        fflush(stdin);
+        scanf("%c", &a[i].gender);
+        printf("The student's number: ");
+        scanf("%d", &a[i].num);
+    }
+
+    for (int i = 0; i < studentsN; i++)
+    {
+        printf("%d. student's card:\nName:%s\nAge:%d\nGender:%c\nNum:%d",i+1,a[i].name,a[i].age,a[i].gender,a[i].num);
+    }
+    
+    
+}
