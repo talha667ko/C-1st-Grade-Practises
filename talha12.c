f52ac8bf51e61a1da94283da06a922511f95905c
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2024-05-30 16:37:35 +0300

    Update talha12.c

--- talha12.c
+++ talha12.c
@@ -3,21 +3,32 @@
 #include <ctype.h>
 #include <string.h>
 
-#define fileway "text.txt"
+/*#define fileway "text.txt"
 
 typedef struct Bank_account{
     char Iban[8];
-    int ID[10];
+    char ID[10];
     float money;
 }Account;
 void money_storage(struct Bank_account *bptr)
 {
+    FILE *fptr;
+    fptr = fopen(fileway, "r");
+    if(fptr == NULL)
+        printf("The file haven't been open!!\n");
 
-    while(fscanf(bptr,"%d %s %f",bptr->ID,bptr->Iban,bptr->money) != NULL){
-            fscanf(bptr,"%d %s %f",bptr->ID,bptr->Iban,&bptr->money);
+    char str[200];
+    char *sentence;
+    while(fgets(str,100,fptr) != NULL){
+            sentence = strtok(str," ");
+            strcpy(bptr->ID,sentence);
+            sentence = strtok(NULL," ");
+            strcpy(bptr->Iban,sentence);
+            sentence = strtok(NULL," ");
+            bptr->money = atof(sentence);
             bptr++;
     }
-
+    fclose(fptr);
 }
 void money_exchange(struct Bank_account *bptr)
 {
@@ -25,29 +36,42 @@ void money_exchange(struct Bank_account *bptr)
     char reciving[8];
     float mny;
     printf("From who want you transfer (IBAN):");
-    fgets(sending,8,stdin);
+    scanf("%s",&sending);
     printf("To who want you transfer (IBAN):");
     fflush(stdin);
-    fgets(reciving,8,stdin);
+    scanf("%s",&reciving);
     printf("How many money want you transfer:");
     scanf("%f",&mny);
 
-    
+    for(int y=0; y <3;y++){
+        if(strcmp(sending,bptr->Iban) == 0)
+            bptr->money = bptr->money - mny;
 
+        if(strcmp(reciving,bptr->Iban) == 0)
+            bptr->money = bptr->money + mny;
+            bptr++;
+    }
+}
+void update(struct Bank_account *bptr)
+{
+    FILE *fptr;
+    fptr = fopen(fileway, "w");
+    if(fptr == NULL)
+        printf("The file haven't been open!!\n");
+
+    for(int y=0; y < 3; y++){
+        fprintf(fptr,"%s %s %f\n",bptr->ID,bptr->Iban,bptr->money);
+        bptr++;
+    }
+    fclose(fptr);
 }
 int main()
 {
     Account compte[3];
-    FILE *fptr;
-    fptr = fopen(fileway, "r");
-
-    if(fptr == NULL)
-        printf("The file haven't been open!!\n");
 
     money_storage(&compte);
-    fptr = fopen(fileway, "r");
     money_exchange(&compte);
+    update(compte);
 
-    fclose(fptr);
     return 0;
-}
+}*/
